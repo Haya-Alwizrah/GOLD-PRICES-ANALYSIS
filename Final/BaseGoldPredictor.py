be2691339294c98ab64d169234491b39ba1c2652
HayaAlwizrah <hayaalwizrah@gmail.com>
2026-08-11 03:32:09 +0300

    .

---
+++ Final/BaseGoldPredictor.py
@@ -0,0 +1,156 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+SEED = 42
+tf.random.set_seed(SEED)
+np.random.seed(SEED)
+
+class BaseGoldPredictor:
+    def __init__(self, model_name):
+        self.model = None
+        self.features = None
+        self.history = None
+        self.best_config = None
+        self.tuning_results = []
+        self.model_name = model_name
+
+    def _build_model(self, input_shape, units1=128, units2=64, units3=32, dropout1=0.3, dropout2=0.2, l2reg=1e-3, lr=1e-3):
+        model = tf.keras.Sequential([
+                    tf.keras.layers.Input(shape=(input_shape,)),
+                    tf.keras.layers.Dense(units1, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(l2reg)),
+                    tf.keras.layers.Dropout(dropout1),
+                    tf.keras.layers.Dense(units2, activation="relu", kernel_regularizer=tf.keras.regularizers.l2(l2reg)),
+                    tf.keras.layers.Dropout(dropout2),
+                    tf.keras.layers.Dense(units3, activation="relu"),
+                    tf.keras.layers.Dense(1, activation="linear")
+                ])
+
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(lr),
+            loss="mae",
+            metrics=["mae", "mse"]
+        )
+
+        return model
+
+    def tune(self, X_train, y_train, X_val, y_val, configs, features=None, epochs=80, batch_size=32, patience=10):
+        self.features = features
+        self.tuning_results = []
+
+        for i, config in enumerate(configs):
+            tf.keras.backend.clear_session()
+            model = self._build_model(input_shape=X_train.shape[1],**config)
+
+            early_stop = tf.keras.callbacks.EarlyStopping(
+                monitor="val_loss",
+                patience=patience,
+                restore_best_weights=True
+            )
+
+            history = model.fit(
+                X_train,
+                y_train,
+                validation_data=(X_val, y_val),
+                epochs=epochs,
+                batch_size=batch_size,
+                callbacks=[early_stop],
+                verbose=0
+            )
+
+            best_val_mae = min(history.history["val_mae"])
+            self.tuning_results.append({
+                "config": config,
+                "val_mae": best_val_mae
+            })
+
+            print(f"Config {i}: {config} -> val_mae={best_val_mae:.4f}")
+
+        # Select best configuration
+        self.best_config = min(self.tuning_results, key=lambda x: x["val_mae"])["config"]
+        print("\nBest config:")
+        print(self.best_config)
+
+        return self.best_config
+
+    def train(self, X_train, y_train, X_val, y_val, configs, features=None, best_config=None, epochs=300, batch_size=32, patience=20):
+        self.features = features
+        if best_config is None:
+            if self.best_config is None:
+                self.tune(X_train, y_train, X_val, y_val, configs, epochs, batch_size, patience)
+            best_config = self.best_config
+
+        self.best_config = best_config
+        tf.keras.backend.clear_session()
+
+        self.model = self._build_model(input_shape=X_train.shape[1], **best_config)
+
+        callbacks = [
+            tf.keras.callbacks.EarlyStopping(
+                monitor="val_loss",
+                patience=patience,
+                restore_best_weights=True
+            ),
+
+            tf.keras.callbacks.ModelCheckpoint(
+                f"best_{self.model_name.lower().replace(' ', '_')}_gold_model.keras",
+                monitor="val_loss",
+                save_best_only=True
+            ),
+
+            tf.keras.callbacks.ReduceLROnPlateau(
+                monitor="val_loss",
+                factor=0.5,
+                patience=8,
+                min_lr=1e-6
+            )
+        ]
+
+        history = self.model.fit(
+            X_train,
+            y_train,
+            validation_data=(X_val, y_val),
+            epochs=epochs,
+            batch_size=batch_size,
+            callbacks=callbacks,
+            verbose=1
+        )
+
+        self.history = history.history
+        return history
+
+    def evaluate(self, X_test, y_test):
+
+        y_pred = self.model.predict(X_test, verbose=0).flatten()
+
+        rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+        mae = mean_absolute_error(y_test, y_pred)
+        r2 = r2_score(y_test, y_pred)
+
+        print(f"\n{self.model_name} Results")
+        print(f"RMSE : {rmse:.3f}")
+        print(f"MAE  : {mae:.3f}")
+        print(f"R²   : {r2:.4f}")
+
+        plt.figure(figsize=(10, 4))
+        plt.plot(self.history["loss"], label="Training Loss")
+        plt.plot(self.history["val_loss"], label="Validation Loss")
+        plt.xlabel("Epoch")
+        plt.ylabel("MAE")
+        plt.title(f"{self.model_name} Model Training History")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+        return {
+            "RMSE": rmse,
+            "MAE": mae,
+            "R2": r2
+        }
+
+    def save(self, path):
+        self.model.save(path)
+
+    def load(self, path):
+        self.model = tf.keras.models.load_model(path)
--- Final/GoldPricePredictor.py
+++ Final/GoldPricePredictor.py
@@ -1,120 +1,93 @@
-import numpy as np
-import tensorflow as tf
-
 from RecursiveGoldPredictor import RecursiveGoldPredictor
 from DirectGoldPredictor import DirectGoldPredictor
 
-SEED = 42
-tf.random.set_seed(SEED)
-np.random.seed(SEED)
-
 class GoldPricePredictor:
+    CONFIGS = [
+        dict(
+            units1=64,
+            units2=32,
+            units3=16,
+            dropout1=0.2,
+            dropout2=0.1,
+            l2reg=1e-4,
+            lr=1e-3
+        ),
+        dict(
+            units1=128,
+            units2=64,
+            units3=32,
+            dropout1=0.3,
+            dropout2=0.2,
+            l2reg=1e-3,
+            lr=1e-3
+        ),
+        dict(
+            units1=128,
+            units2=64,
+            units3=32,
+            dropout1=0.4,
+            dropout2=0.3,
+            l2reg=1e-3,
+            lr=5e-4
+        ),
+        dict(
+            units1=256,
+            units2=128,
+            units3=64,
+            dropout1=0.4,
+            dropout2=0.3,
+            l2reg=1e-2,
+            lr=1e-3
+        )
+    ]
+
     def __init__(self):
         self.recursive_model = RecursiveGoldPredictor()
         self.direct_model = DirectGoldPredictor()
 
-        self.results = {}
-
-        self.configs = [
-            dict(
-                units1=64,
-                units2=32,
-                units3=16,
-                dropout1=0.2,
-                dropout2=0.1,
-                l2reg=1e-4,
-                lr=1e-3
-            ),
-            dict(
-                units1=128,
-                units2=64,
-                units3=32,
-                dropout1=0.3,
-                dropout2=0.2,
-                l2reg=1e-3,
-                lr=1e-3
-            ),
-            dict(
-                units1=128,
-                units2=64,
-                units3=32,
-                dropout1=0.4,
-                dropout2=0.3,
-                l2reg=1e-3,
-                lr=5e-4
-            ),
-            dict(
-                units1=256,
-                units2=128,
-                units3=64,
-                dropout1=0.4,
-                dropout2=0.3,
-                l2reg=1e-2,
-                lr=1e-3
-            )
-        ]
-
-
-    def train_recursive(self, X_train, y_train, X_val, y_val, epochs=300, batch_size=32, tune_epochs=80, tune_patience=10, patience=20):
-        print("--------- Training Recursive Model --------------")
-
-        self.recursive_model.tune(
+# ---------------------------------------------------------------------------
+    def train_model(self, model, X_train, y_train, X_val, y_val, epochs=300, batch_size=32, tune_epochs=80, tune_patience=10, patience=20):
+        model.tune(
             X_train=X_train,
             y_train=y_train,
             X_val=X_val,
             y_val=y_val,
-            configs=self.configs,
+            configs=self.CONFIGS,
             epochs=tune_epochs,
             batch_size=batch_size,
             patience=tune_patience
         )
 
-        self.recursive_model.train(
+        model.train(
             X_train=X_train,
             y_train=y_train,
             X_val=X_val,
             y_val=y_val,
-            configs=self.configs,
+            configs=self.CONFIGS,
             epochs=epochs,
             batch_size=batch_size,
             patience=patience
         )
 
-    def train_direct(self, X_train, y_train, X_val, y_val, epochs=300, batch_size=32, tune_epochs=80, tune_patience=10, patience=20):
-        print(" ------------ Training Direct Model---------- ")
+    def train_recursive(self, *args, **kwargs):
+        print("Training Recursive Model")
+        self.train_model(self.recursive_model, *args, **kwargs)
 
-        self.direct_model.tune(
-            X_train=X_train,
-            y_train=y_train,
-            X_val=X_val,
-            y_val=y_val,
-            configs=self.configs,
-            epochs=tune_epochs,
-            batch_size=batch_size,
-            patience=tune_patience
-        )
+    def train_direct(self, *args, **kwargs):
+        print("Training Direct Model")
+        self.train_model(self.direct_model, *args, **kwargs)
 
-        self.direct_model.train(
-            X_train=X_train,
-            y_train=y_train,
-            X_val=X_val,
-            y_val=y_val,
-            configs=self.configs,
-            epochs=epochs,
-            batch_size=batch_size,
-            patience=patience
-        )
+# ----------------------------------------------------------------------------
 
     def evaluate_recursive(self, X_test, y_test):
         result = self.recursive_model.evaluate(X_test, y_test)
-        self.results["recursive"] = result
         return result
 
     def evaluate_direct(self, X_test, y_test):
         result = self.direct_model.evaluate(X_test, y_test)
-        self.results["direct"] = result
         return result
 
+# ----------------------------------------------------------------------------
     def predict_recursive(self, latest_data, historical_data, scaler_X, days=21):
         return self.recursive_model.forecast(
             latest_data=latest_data,
@@ -123,26 +96,22 @@ class GoldPricePredictor:
             days=days
         )
 
-    def predict_direct(self, latest_data, X_columns, scaler):
-        return self.direct_model.predict(
+    def predict_direct(self, latest_data, scaler_X, current_price):
+        return self.direct_model.predict_after_21_days(
             latest_data=latest_data,
-            X_columns=X_columns,
-            scaler=scaler
+            scaler_X=scaler_X,
+            current_price=current_price
         )
+    
+# ----------------------------------------------------------------------------
+    def load_models(self, recursive_path="recursive_gold_model.keras", direct_path="direct_gold_model.keras"):
+        self.recursive_model.load(recursive_path)
+        self.direct_model.load(direct_path)
+        print("Models loaded successfully.")
+
 
     def save_models(self, recursive_path="recursive_gold_model.keras", direct_path="direct_gold_model.keras"):
         self.recursive_model.save(recursive_path)
         self.direct_model.save(direct_path)
 
-        print("Models saved successfully.")
-
-    def load_models(self, recursive_path=None, direct_path=None):
-        if recursive_path is not None:
-            self.recursive_model.load(recursive_path)
-
-        if direct_path is not None:
-            self.direct_model.load(direct_path)
-
-        print("Models loaded successfully.")
-
-        
+        print("Models saved successfully.") 
